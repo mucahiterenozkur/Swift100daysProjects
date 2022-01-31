bb263f3d834a741c5b35e51ea0e9eee2e853fe5f
Mücahit Eren Özkur <mucahiterenozkur@hotmail.com>
2022-01-31 14:47:42 +0300

    project 7 is added

---
+++ Project7/Project7/Petition.swift
@@ -0,0 +1,14 @@
+//
+//  Petition.swift
+//  Project7
+//
+//  Created by Mücahit Eren Özkur on 31.01.2022.
+//
+
+import Foundation
+
+struct Petition: Codable {
+    var title: String
+    var body: String
+    var signatureCount: Int
+}
---
+++ Project7/Project7/Petitions.swift
@@ -0,0 +1,12 @@
+//
+//  Petitions.swift
+//  Project7
+//
+//  Created by Mücahit Eren Özkur on 31.01.2022.
+//
+
+import Foundation
+
+struct Petitions: Codable {
+    var results: [Petition]
+}
